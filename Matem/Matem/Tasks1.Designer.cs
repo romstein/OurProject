fbff32f80b2202fcc19374b3727a0f842a09f06b
romstein <roman.goryunov@mail.ru>
2020-10-21 11:42:59 +0300

    Добавил много фигни

--- Matem/Matem/Tasks1.Designer.cs
+++ Matem/Matem/Tasks1.Designer.cs
@@ -28,25 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tasks1));
             this.panel2 = new System.Windows.Forms.Panel();
             this.closeButton = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.AddQuestion = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.сОднимВариантомОтветаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.KolichestvoAnswer = new System.Windows.Forms.ToolStripTextBox();
             this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.Yellow;
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 450);
-            this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
@@ -56,7 +51,7 @@
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 91);
+            this.panel2.Size = new System.Drawing.Size(801, 90);
             this.panel2.TabIndex = 0;
             // 
             // closeButton
@@ -64,7 +59,7 @@
             this.closeButton.AutoSize = true;
             this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.closeButton.Location = new System.Drawing.Point(774, 0);
+            this.closeButton.Location = new System.Drawing.Point(750, 0);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(26, 25);
             this.closeButton.TabIndex = 1;
@@ -80,44 +75,101 @@
             this.label1.ForeColor = System.Drawing.Color.Green;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(800, 91);
+            this.label1.Size = new System.Drawing.Size(801, 90);
             this.label1.TabIndex = 0;
             this.label1.Text = "Задания для учеников";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label2
+            // panel1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(216, 207);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(325, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Здесь преподователь будет создавать вопросы";
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.Controls.Add(this.toolStrip1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(801, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AddQuestion,
+            this.toolStripSplitButton1,
+            this.KolichestvoAnswer});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 90);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(801, 27);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // AddQuestion
+            // 
+            this.AddQuestion.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.AddQuestion.Image = ((System.Drawing.Image)(resources.GetObject("AddQuestion.Image")));
+            this.AddQuestion.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.AddQuestion.Name = "AddQuestion";
+            this.AddQuestion.Size = new System.Drawing.Size(24, 24);
+            this.AddQuestion.Text = "Добавить вопрос";
+            this.AddQuestion.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.BackColor = System.Drawing.Color.Yellow;
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сОднимВариантомОтветаToolStripMenuItem});
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(39, 24);
+            this.toolStripSplitButton1.Text = "Выберите тип вопроса";
+            // 
+            // сОднимВариантомОтветаToolStripMenuItem
+            // 
+            this.сОднимВариантомОтветаToolStripMenuItem.Name = "сОднимВариантомОтветаToolStripMenuItem";
+            this.сОднимВариантомОтветаToolStripMenuItem.Size = new System.Drawing.Size(273, 26);
+            this.сОднимВариантомОтветаToolStripMenuItem.Text = "С одним вариантом ответа";
+            this.сОднимВариантомОтветаToolStripMenuItem.Click += new System.EventHandler(this.сОднимВариантомОтветаToolStripMenuItem_Click);
+            // 
+            // KolichestvoAnswer
+            // 
+            this.KolichestvoAnswer.Name = "KolichestvoAnswer";
+            this.KolichestvoAnswer.Size = new System.Drawing.Size(205, 27);
+            this.KolichestvoAnswer.Text = "Введите количество ответов";
+            this.KolichestvoAnswer.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox2_KeyDown);
+            this.KolichestvoAnswer.Click += new System.EventHandler(this.toolStripTextBox2_Click);
             // 
             // Tasks1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(797, 450);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Tasks1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tasks1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label closeButton;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton AddQuestion;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem сОднимВариантомОтветаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox KolichestvoAnswer;
     }
 }
--- Matem/Matem/Tasks1.cs
+++ Matem/Matem/Tasks1.cs
@@ -12,6 +12,25 @@ namespace Matem
 {
     public partial class Tasks1 : Form
     {
+        public int currentIndexRadio = 0;
+        public int currentRadioButton = 0;
+        RadioButton[] radio = new RadioButton[1000];
+
+        public int Nans = 0;
+        public int localHeight = 0;
+        public int flagVyborOtveta = 0;
+
+        public int panelLokation = 110;
+        public int PanelConstanta=0;
+        public Panel[] panel = new Panel[100];        
+
+        TextBox[] textTask = new TextBox[100];
+        public int currentIndexTextTask = 0;
+
+        TextBox[] textAnswer = new TextBox[1000];
+        public int currentIndexTextAnswer = 0;
+
+
         public Tasks1()
         {
             InitializeComponent();
@@ -30,6 +49,91 @@ namespace Matem
         private void closeButton_MouseClick(object sender, MouseEventArgs e)
         {
             Application.Exit();
+        }        
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            panel[PanelConstanta] = new Panel();
+            panel[PanelConstanta].Width = panel1.Width;
+            panel[PanelConstanta].Height = 0;
+            panel[PanelConstanta].Location = new Point(0, panelLokation);
+            panel[PanelConstanta].BackColor = Color.Green;
+
+            textTask[currentIndexTextTask] = new TextBox();
+            textTask[currentIndexTextTask].Multiline = true;
+            textTask[currentIndexTextTask].Width = panel1.Width;
+            textTask[currentIndexTextTask].Height = 20;
+            panel[PanelConstanta].Height += textTask[currentIndexTextTask].Height;
+            panel[PanelConstanta].Controls.Add(textTask[currentIndexTextTask]);
+            localHeight += textTask[currentIndexTextTask].Height;
+            
+            
+            Nans = int.Parse(KolichestvoAnswer.Text);
+            if (flagVyborOtveta==1)
+            {
+                for (int i = currentIndexRadio; i < currentIndexRadio + Nans; i++)
+                {
+                    radio[i] = new RadioButton();
+                    radio[i].Width = panel1.Width-200;
+                    radio[i].Height = 20;
+                    radio[i].Location = new Point(0, localHeight);
+                    radio[i].BackColor = Color.Red;
+                    panel[PanelConstanta].Height += radio[i].Height;
+                    panel[PanelConstanta].Controls.Add(radio[i]);
+                    textAnswer[currentIndexTextAnswer] = new TextBox();
+                    textAnswer[currentIndexTextAnswer].Width = 200;
+                    textAnswer[currentIndexTextAnswer].Height = 20;
+                    textAnswer[currentIndexTextAnswer].Location = new Point(radio[i].Width, localHeight);
+                    textAnswer[currentIndexTextAnswer].KeyDown += new System.Windows.Forms.KeyEventHandler(AddAnswer);
+                    panel[PanelConstanta].Controls.Add(textAnswer[currentIndexTextAnswer]);
+                    localHeight += radio[i].Height;
+                    currentIndexTextAnswer++;
+                }
+            }
+            
+            panel1.Controls.Add(panel[PanelConstanta]);
+
+            currentIndexRadio += Nans;
+            currentIndexTextTask++;            
+            localHeight += 5;
+            PanelConstanta++;
+            panelLokation += localHeight;
+            localHeight = 0;
+            
+        }
+
+        private void AddAnswer(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Enter)
+            {
+                for (int i=0;i<1000;i++)
+                {
+                    if (textAnswer[i].Focused)
+                    {
+                        radio[i].Text = textAnswer[i].Text;
+                        break;
+                    }
+                }
+                                
+            }
+        }
+        
+        private void toolStripTextBox2_Click(object sender, EventArgs e)
+        {
+            KolichestvoAnswer.Text = "";
+        }
+
+        private void toolStripTextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Enter)
+            {
+                PanelConstanta = int.Parse(KolichestvoAnswer.Text);
+            }
+        }        
+
+        private void сОднимВариантомОтветаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            flagVyborOtveta = 1;
         }
     }
 }
